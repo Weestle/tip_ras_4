9cb49fce10a4e4f2ae221a5601740a2b9281f5a9
LASOV <44479103+Weestle@users.noreply.github.com>
2022-10-16 02:09:54 +0300

    1

--- main.py
+++ main.py
@@ -2,22 +2,21 @@ import math
 
 # Вариант 5
 
-# K = 4.0
-# L = 2.4
-
-
-K = float(input('K = '))
-L = float(input('L = '))
+K = 4.0
+L = 2.4
 a = (K - L) / 2
 b = K + L
+
 
 def exact_I(x):
     global K, L
     return 0.5 * math.log(x ** 2 + x + K) + (L - 0.5) / (K - 0.25) ** 0.5 * math.atan((x + 0.5) / (K - 0.25) ** 0.5)
 
+
 def range_exact_I():
     global a, b
     return exact_I(b) - exact_I(a)
+
 
 def F(x):
     global K, L
@@ -51,8 +50,6 @@ def parabola(n):
     return I
 
 
-
-
 def gauss(n):
     ti4 = [0.861136, 0.339981]
     ti6 = [0.932464, 0.661209, 0.238619]
@@ -60,7 +57,6 @@ def gauss(n):
     A4 = [0.347854, 0.652145]
     A6 = [0.171324, 0.360761, 0.467913]
     A8 = [0.101228, 0.222381, 0.313706, 0.362683]
-
     if n == 4:
         ti = ti4
         A = A4
@@ -70,13 +66,10 @@ def gauss(n):
     if n == 8:
         ti = ti8
         A = A8
-    # print(A)
     I = 0
     for i in range(int(n / 2)):
         I += A[i] * F((a + b) / 2 + (b - a) / 2 * ti[i])
         I += A[i] * F((a + b) / 2 - (b - a) / 2 * ti[i])
-        # print(A[i], F((a + b) / 2 + (b - a) / 2 * ti[i]))
-        # print(A[i], F((a + b) / 2 - (b - a) / 2 * ti[i]))
     I *= (b - a) / 2
     return I
 
@@ -88,3 +81,4 @@ print('%-10s %-20s %-20s %-20s' % ('Iтр', trapezoid(4), trapezoid(6), trapezoi
 print('%-10s %-20s %-20s %-20s' % ('Iпар', parabola(4), parabola(6), parabola(8)))
 print('%-10s %-20s %-20s %-20s' % ('Ig', gauss(4), gauss(6), gauss(8)))
 print('%-10s %-20s %-20s %-20s' % ('Ireal', range_exact_I(), range_exact_I(), range_exact_I()))
+input()
