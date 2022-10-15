708a88800253bd54c9536334507abb3daace30c0
LASOV <44479103+Weestle@users.noreply.github.com>
2022-10-16 01:48:27 +0300

    1

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ import math
 
 K = 4.0
 L = 2.4
-# a = (K - L) / 2
-# b = K + L
-a = 0
-b = 1
-
+a = (K - L) / 2
+b = K + L
+# a = 0
+# b = 1
 
 def exact_I(x):
     global K, L
     return 0.5 * math.log(x ** 2 + x + K) + (L - 0.5) / (K - 0.25) ** 0.5 * math.atan((x + 0.5) / (K - 0.25) ** 0.5)
 
+def range_exact_I():
+    global a, b
+    return exact_I(b) - exact_I(a)
 
 def F(x):
     global K, L
-    return 1 / (1 + x)
-    # return (x + L) / (x ** 2 + x + K)
+    return (x + L) / (x ** 2 + x + K)
 
 
 def trapezoid(n):
@@ -28,7 +29,7 @@ def trapezoid(n):
     for i in range(1, n):
         x = a + i * h
         I += 2 * F(x)
-    I = (I + a + b) * h / 2
+    I = (I + F(a) + F(b)) * h / 2
     return I
 
 
@@ -74,11 +75,14 @@ def gauss(n):
         I += A[i] * F((a + b) / 2 - (b - a) / 2 * ti[i])
         # print(A[i], F((a + b) / 2 + (b - a) / 2 * ti[i]))
         # print(A[i], F((a + b) / 2 - (b - a) / 2 * ti[i]))
-    I *= 0.5
+    I *= (b - a) / 2
     return I
 
 
-print('%-10s %-20s %-20s %-20s' % ('Трапеция', '4', '6', '8'))
+trapezoid(4)
+
+print('%-10s %-20s %-20s %-20s' % ('n', '4', '6', '8'))
 print('%-10s %-20s %-20s %-20s' % ('Iтр', trapezoid(4), trapezoid(6), trapezoid(8)))
 print('%-10s %-20s %-20s %-20s' % ('Iпар', parabola(4), parabola(6), parabola(8)))
 print('%-10s %-20s %-20s %-20s' % ('Ig', gauss(4), gauss(6), gauss(8)))
+print('%-10s %-20s %-20s %-20s' % ('Ireal', range_exact_I(), range_exact_I(), range_exact_I()))
